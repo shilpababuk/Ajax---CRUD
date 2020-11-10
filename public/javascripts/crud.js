b3c2bf5aa060c3d445742264b59fbe012d3221b9
shilpa <shilpaksbabu@gmail.com>
2020-11-10 15:41:54 +0530

    Delete a user

--- public/javascripts/crud.js
+++ public/javascripts/crud.js
@@ -1,3 +1,79 @@
+/* comments for list view */
+// $(()=> {
+
+
+
+//     $('#alertDiv').hide();
+
+//    /* attach a submit handler to the form */
+//    $("#addUserForm").submit(function(event) {
+//     console.log(`inside submit`)
+
+//  /* stop form from submitting normally */
+//  event.preventDefault();
+
+//  /* get some values from elements on the page: */
+//  let $form = $(this)
+//  let name = $('#name').val()
+//  let email = $('#email').val()
+//  let url = $form.attr('action');
+
+//  /* Send the data using post */
+//  console.log(name, email, url)
+ 
+//     $.post(url, {
+//         name: name,
+//         email: email
+//     }, (data, status, xhr) => {
+//          if (status == "success") {
+//             console.log(data);
+//             console.log(status);
+//             $("#listGroup").prepend('<li class="list-group-item" >Name: <b>' + data.name + '</b> <br> Email: <b>' + data.email + '</b> </li>');
+//             // Reset form input after successfull submission
+//             $('#addUserForm').each(function () {
+//                 this.reset();
+//                 $('#alertDiv').show().text('User '+ data.name +' added successfully!').fadeOut(8000)
+//             });
+
+//         }
+
+//     });
+
+
+// });
+
+//   $.get('/allUsers',(users, status, xhr)=>{
+//     if(users) {
+//         console.log(users);
+//         $.each(users, (key,obj)=>{
+//           console.log(key);
+//           console.log(obj);
+//           $("#listGroup").prepend('<li class="list-group-item" >Name : <b>'+ obj.name + '</b> <br> Email : <b>'+ obj.email +'</b> </li>');
+//         })
+//     }
+      
+//   })
+
+//   $(document).ready(function()
+//     {
+//         $(".delete_video").click(function()
+//         {
+//             var del_id = $(this).attr('id');
+//             $.ajax({
+//                 type:'POST',
+//                 url:'delete.php',
+//                 data:'delete_id='+del_id,
+//                 success: function(data)
+//                 {
+//                     //reload page
+//                     location.reload();
+//                 }
+//             });
+//         });
+//     });
+
+// })
+
 $(()=> {
 
     $('#alertDiv').hide();
@@ -25,10 +101,11 @@ $(()=> {
          if (status == "success") {
             console.log(data);
             console.log(status);
-            $("#listGroup").prepend('<li class="list-group-item" >Name: <b>' + data.name + '</b> <br> Email: <b>' + data.email + '</b> </li>');
+            $("#tableGroup").append('<td class="table-group-item" > ' + data.name + ' </td> <td> ' + data.email + '</td> <td> <button class="btn btn-primary edit"type="button"> Edit </button> </td> <td> <button class="btn btn-danger remove" type="button">Remove</button> </td> ');
             // Reset form input after successfull submission
             $('#addUserForm').each(function () {
                 this.reset();
+                location.reload('/')
                 $('#alertDiv').show().text('User '+ data.name +' added successfully!').fadeOut(8000)
             });
 
@@ -43,32 +120,40 @@ $(()=> {
     if(users) {
         console.log(users);
         $.each(users, (key,obj)=>{
-          console.log(key);
-          console.log(obj);
-          $("#listGroup").prepend('<li class="list-group-item" >Name : <b>'+ obj.name + '</b> <br> Email : <b>'+ obj.email +'</b> </li>');
+            
+            console.log(key);
+            console.log(obj.name); 
+
+            $("#tblOne > tbody").append('<tr> <td>'+obj.name+'</td><td>'+ obj.email+'</td><td> <button class="btn btn-primary edit"type="button">Edit</button></td><td><button class="btn btn-danger remove" id="'+obj._id+' "type="button">Remove</button></td></tr>');
+        
+     
         })
+        
     }
       
   })
 
-  $(document).ready(function()
-    {
-        $(".delete_video").click(function()
-        {
-            var del_id = $(this).attr('id');
-            $.ajax({
-                type:'POST',
-                url:'delete.php',
-                data:'delete_id='+del_id,
-                success: function(data)
-                {
-                    //reload page
-                    location.reload();
-                }
-            });
-        });
-    });
+  // jQuery button click event to edit a row. 
+  $('#tblOne').on('click', '.edit', function () {
+      $(this).hide();
+  })
 
+  // jQuery button click event to remove a row. 
+  $('#tblOne').on('click', '.remove', function (e) {
+    console.log(e.target.id);
+    console.log(e.target.name);
+
+   alert('Are you sure you want to delete:'+ e.target.name)
+
+    $.get(`/deleteUser/${e.target.id}`,(response,status,xhr)=>{
+        console.log(response);
+        if(response){
+            location.reload('/')
+        }
+    })
+  })
+  
 })
 
+ 
  
